e9c6da8e6c0930a37e1e8aecb886d76df6fc1f73
mukhammademin <mukhammademin19@icloud.com>
2025-08-05 23:18:46 +0500

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Mukhammademin Eminov on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
