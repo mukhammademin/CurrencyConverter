b46c3865fbff69f93ee4f740b9ecaca5263acd2c
mukhammademin <mukhammademin19@icloud.com>
2025-08-06 23:40:32 +0500

    Initial commit: Currency converter

--- Currency/ContentView.swift
+++ Currency/ContentView.swift
@@ -8,15 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var input = "" //for TextField
+    let usdRate = 12000.0
+    let eurRate = 13000.0
+    
+    
+    @State private var selectedCurrency = "USD -> UZS"
+    let currencies = ["UZS → USD", "UZS → EUR", "USD → UZS", "EUR → UZS"]
+    
+    var convertedAmount: Double {
+        guard let amount = Double(input) else { return 0 }
+        
+        switch selectedCurrency {
+        case "UZS → USD": return amount / usdRate
+        case "UZS → EUR": return amount / eurRate
+        case "USD → UZS": return amount * usdRate
+        case "EUR → UZS": return amount * eurRate
+        default : return 0
         }
-        .padding()
     }
+    var outputCurrencySymbol: String {
+        switch selectedCurrency {
+        case "UZS → USD", "USD → UZS":
+            return "USD"
+        case "UZS → EUR", "EUR → UZS":
+            return "EUR"
+        default:
+            return ""
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section(header: Text("Enter amount")) {
+                    HStack {
+                        TextField("e.g 1000", text: $input)
+                            .keyboardType(.decimalPad)
+                            .submitLabel(.done)
+                        Spacer()
+                        Text(selectedCurrency.prefix(3))
+                            .foregroundColor(.gray)
+                    }
+                }
+                
+                Section(header: Text("Currency")) {
+                    
+                    Picker("Select currency", selection: $selectedCurrency) {
+                        ForEach(currencies, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                }
+                
+                Section(header: Text("Converted amount")) {
+                    if input.isEmpty {
+                        Text("Enter amount to convert")
+                    } else {
+                        Text("\(convertedAmount, specifier: convertedAmount <= 0.01 ? "%.4f" : "%.2f") \(outputCurrencySymbol)")
+                    }
+                }
+            }
+            
+            .navigationTitle("Currency converter")
+        }
+        
+    }
+
 }
 
 #Preview {
